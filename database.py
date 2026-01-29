9ea234b7457f33e439d6d49818cdedf6fcdd29b4
Benjamin3600 <kwarteng3600@gmail.com>
2026-01-29 10:36:01 +0000

    Update database.py

--- database.py
+++ database.py
@@ -6,8 +6,4 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 def get_conn():
     if not DATABASE_URL:
         raise RuntimeError("DATABASE_URL not set")
-
-    return psycopg2.connect(
-        DATABASE_URL,
-        sslmode="require"
-    )
+    return psycopg2.connect(DATABASE_URL, sslmode="require")
